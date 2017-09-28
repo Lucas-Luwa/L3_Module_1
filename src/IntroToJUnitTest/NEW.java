b194511379e22518ec97cc4be10f2201af51ced2
TJHL <league@iMac.attlocal.net>
2017-09-27 18:27:44 -0700

    New 9/27/17
    IntroToJUnitTest(IP)

---
+++ src/IntroToJUnitTest/NEW.java
@@ -0,0 +1,17 @@
+package IntroToJUnitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NEW {
+ 
+	@Test
+	public void test() {
+		assertEquals(23,2);
+	}
+	void Create(int x, int y) {
+	
+	}
+
+}
